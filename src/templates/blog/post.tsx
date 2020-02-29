14139d416d3c4471ebc0668bfb421117505cd784
mark sauer-utley <mark.utley.713@gmail.com>
2020-02-29 09:25:18 -0500

    add meta description to blog posts

--- src/templates/blog/post.tsx
+++ src/templates/blog/post.tsx
@@ -6,10 +6,10 @@ import { PageProps } from '../../types';
 
 const Post: React.FC<PageProps> = (props) => {
   const { post, next, previous } = props.pageContext;
-  const { title } = post.frontmatter;
+  const { title, blurb } = post.frontmatter;
   return (
     <Layout>
-      <SEO title={title} />
+      <SEO title={title} description={blurb} />
       <h1>{title}</h1>
       <div style={{ background: '#ffffffaa', padding: '10px' }} dangerouslySetInnerHTML={{ __html: post.html }}></div>
       <ReadMore next={next} previous={previous} />
